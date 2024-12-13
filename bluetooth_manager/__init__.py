6022b95bd78b4773b622a8f0f20b119a2abb9d0c
HaudinFlorence <haudin.florence@gmail.com>
2024-12-13 15:06:06 +0100

    Change all references to jupyterlab-web-bluetooth-manager to bluetooth-manager.

--- bluetooth_manager/__init__.py
+++ bluetooth_manager/__init__.py
@@ -12,5 +12,5 @@ except ImportError:
 def _jupyter_labextension_paths():
     return [{
         "src": "labextension",
-        "dest": "jupyterlab-web-bluetooth-manager"
+        "dest": "bluetooth-manager"
     }]
