d8615fe0de0cf014f8d0de06bd8c1adea780e7ab
HaudinFlorence <haudin.florence@gmail.com>
2024-12-19 14:29:45 +0100

    Add a dropdown list and remove the model/view pattern to populate the body of the dialog.

--- src/bluetooth-extension/index.ts
+++ src/bluetooth-extension/index.ts
@@ -14,8 +14,9 @@ import {
   BluetoothManager
 } from '../bluetooth/BluetoothManager';
 import { Dialog, showDialog } from '@jupyterlab/apputils';
-import { RegistryDialogModel } from './RegistryDialogModel';
-import { RegistryDialogView } from './RegistryDialogView';
+/*import { RegistryDialogModel } from './RegistryDialogModel';
+import { RegistryDialogView } from './RegistryDialogView';*/
+import { Widget } from '@lumino/widgets';
 
 const BluetoothManagerPlugin: JupyterFrontEndPlugin<IBluetoothManager> = {
   id: 'bluetooh-manager:bluetooth-manager-plugin',
@@ -27,6 +28,13 @@ const BluetoothManagerPlugin: JupyterFrontEndPlugin<IBluetoothManager> = {
     return new BluetoothManager();
   }
 };
+/*function getIdentifierList(bluetoothManager: IBluetoothManager) {
+  let identifierList: Array<string> = [];
+  bluetoothManager.registry.itemsList.forEach(item => {
+    identifierList.push(item.identifier);
+  });
+  return identifierList;
+}*/
 
 const BluetoothSidebarPlugin: JupyterFrontEndPlugin<void> = {
   id: 'bluetooh-manager:bluetooth-sidebar-plugin',
@@ -47,21 +55,32 @@ const BluetoothSidebarPlugin: JupyterFrontEndPlugin<void> = {
       'Open Dialog Showing Devices Registry'
     );
     let runningItemsList: Array<IRunningSessions.IRunningItem>;
-    const model = new RegistryDialogModel(bluetoothManager);
-    const view = new RegistryDialogView(model, translator);
-    
+    //const model = new RegistryDialogModel(bluetoothManager);
+    //const view = new RegistryDialogView(model, translator);
+    //const registryIdentifierList = getIdentifierList(bluetoothManager);
     app.commands.addCommand(CommandIDs.openDevicesRegistryDialog, {
       execute: async () => {
         showDialog({
           title: 'Select an item from the registry',
-          body: view,
+          body: new DropDownRegistry(bluetoothManager.registry),
           buttons: [
             Dialog.okButton({ label: 'OK' }),
             Dialog.cancelButton({ label: 'Cancel' })
           ]
-        }).then(result => {
+        }).then(async result => {
+          console.log('result:', result);
           if (result.button.accept) {
-            console.log('OK button clicked');
+            console.log(result.value);
+            bluetoothManager.registry.itemsList.forEach(async item => {
+              if (item.identifier === result.value) {
+                console.log('Found')
+                const device = await bluetoothManager.connectDevice(item);
+                console.log('device:', device);
+              }
+              else {
+                console.warn(`item.identifier = ${item.identifier} and result.value=${result.value}`)
+              }
+            });
           } else {
             console.log('Cancel button clicked');
           }
@@ -105,6 +124,34 @@ const BluetoothSidebarPlugin: JupyterFrontEndPlugin<void> = {
     });
   }
 };
+
+export class DropDownRegistry
+  extends Widget
+  implements Dialog.IBodyWidget<string>
+{
+  constructor(registry: BluetoothManager.DeviceRegistry) {
+    super();
+
+    this._selectList = document.createElement('select');
+    this.node.appendChild(this._selectList);
+    this.registry = registry;
+
+    registry.itemsList.forEach(item => {
+      const option = document.createElement('option');
+      option.value = item.identifier;
+      console.log('option.value:', option.value);
+      option.text = item.identifier;
+      this._selectList.appendChild(option);
+    });
+  }
+
+  getValue(): string {
+    return this._selectList.value;
+  }
+  private _selectList: HTMLSelectElement;
+  public registry: BluetoothManager.DeviceRegistry;
+}
+
 const BluetoothExtensionPlugins: JupyterFrontEndPlugin<any>[] = [
   BluetoothManagerPlugin,
   BluetoothSidebarPlugin
