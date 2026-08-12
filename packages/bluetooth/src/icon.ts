ec70b23f444a2c81dbff2b145f8d4163c941c0d1
Florence Haudin <haudin.florence@gmail.com>
2026-08-12 15:02:56 +0200

    Update package.json and tsconfig.json files to take review comments into account. Restore and update global LICENSE file and add similar files in the packages.

--- packages/bluetooth/src/icon.ts
+++ packages/bluetooth/src/icon.ts
@@ -1,6 +1,6 @@
 import { LabIcon } from '@jupyterlab/ui-components';
-import bluetoothConnectSvgstr from '../style/icons/bluetoothConnect.svg';
-import bluetoothDisconnectSvgstr from '../style/icons/bluetoothDisconnect.svg';
+import bluetoothConnectSvgstr from '@bluetooth-manager/bluetooth/style/icons/bluetoothConnect.svg';
+import bluetoothDisconnectSvgstr from '@bluetooth-manager/bluetooth/style/icons/bluetoothDisconnect.svg';
 
 
 export const BluetoothConnectIcon = new LabIcon({
