8591f3bf48dbf4346056d8c7f0647d20754b40d8
Florence Haudin <haudin.florence@gmail.com>
2026-08-13 14:39:44 +0200

    Fix imports in bluetooth-extension.

--- packages/bluetooth-extension/src/index.ts
+++ packages/bluetooth-extension/src/index.ts
@@ -6,12 +6,7 @@ import { ITranslator } from '@jupyterlab/translation';
 import { IRunningSessions, IRunningSessionManagers } from '@jupyterlab/running';
 import { addIcon, CommandToolbarButton } from '@jupyterlab/ui-components';
 import { Dialog, showDialog } from '@jupyterlab/apputils';
-import { BluetoothDeviceRunningItem } from '@bluetooth-manager/bluetooth/src/BluetoothDeviceRunningItem';
-import {
-  IBluetoothManager,
-  BluetoothManager
-} from '@bluetooth-manager/bluetooth/src/BluetoothManager';
-import { DropDownRegistry } from '@bluetooth-manager/bluetooth/src/DropDownRegistry';
+import { BluetoothDeviceRunningItem, BluetoothManager, DropDownRegistry, IBluetoothManager } from '@bluetooth-manager/bluetooth';
 
 export namespace CommandIDs {
   export const openDeviceRegistryDialog =
