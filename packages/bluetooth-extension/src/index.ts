21f2b36a0c07d53bf421e7b202e875ef6ed0ad9f
Florence Haudin <haudin.florence@gmail.com>
2026-08-13 19:05:23 +0200

    Try to fix jupyter lab console errors.

--- packages/bluetooth-extension/src/index.ts
+++ packages/bluetooth-extension/src/index.ts
@@ -6,7 +6,7 @@ import { ITranslator } from '@jupyterlab/translation';
 import { IRunningSessions, IRunningSessionManagers } from '@jupyterlab/running';
 import { addIcon, CommandToolbarButton } from '@jupyterlab/ui-components';
 import { Dialog, showDialog } from '@jupyterlab/apputils';
-import { BluetoothDeviceRunningItem, BluetoothManager, DropDownRegistry, IBluetoothManager } from '@bluetooth-manager/bluetooth';
+import { BluetoothDeviceRunningItem, BluetoothManager, DropDownRegistry, IBluetoothManager } from '@jupyter-bluetooth-manager/bluetooth';
 
 export namespace CommandIDs {
   export const openDeviceRegistryDialog =
